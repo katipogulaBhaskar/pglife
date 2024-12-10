55244010ff7839c7021a28e00526a459f1e2a062
katipogulaBhaskar <157219070+katipogulaBhaskar@users.noreply.github.com>
2024-12-10 21:27:17 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import router from './src/router/user.router.js';
+import connectToMongoDB from './src/config/dbConnect.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//CORS configuration
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allow cookies to be sent
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use('/api/user', router);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running in port ${PORT}`);
+});
